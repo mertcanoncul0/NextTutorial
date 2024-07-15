b970e526339cf5951dc576a0617ca8f636489569
Mertcan Öncül <mertcanoncul@Mertcan-MacBook-Air.local>
2024-07-15 14:26:11 +0300

    middleware updated

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,36 +1,9 @@
-import Search from "@/app/ui/search"
-import { CreateInvoice } from "@/app/ui/invoices/buttons"
-import { lusitana } from "@/app/ui/fonts"
-import { Suspense } from "react"
-import { InvoicesTableSkeleton } from "@/app/ui/skeletons"
-import CustomersTable from "@/app/ui/customers/table"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "Invoices",
 }
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: {
-    query?: string
-    page?: string
-  }
-}) {
-  const query = searchParams?.query || ""
-  const currentPage = Number(searchParams?.page) || 1
-
-  return (
-    <div className="w-full">
-      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
-        Customers
-      </h1>
-      <Search placeholder="Search customers..." />
-
-      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <CustomersTable query={query} />
-      </Suspense>
-    </div>
-  )
+export default async function Page() {
+  return <div>Customers</div>
 }
--- middleware.ts
+++ middleware.ts
@@ -4,14 +4,5 @@ import { authConfig } from "./auth.config"
 export default NextAuth(authConfig).auth
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
-  ],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 }
